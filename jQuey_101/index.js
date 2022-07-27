f07f672ced99a80388670318e35bfab860549a01
ZaeemT <zaeemtauqir@hotmail.com>
2022-07-28 01:19:09 +0500

    jQuery done.

--- jQuey_101/index.js
+++ jQuey_101/index.js
@@ -1,7 +1,15 @@
 $(document).ready(function() {
     //jQuery in here 
     $(".getRandomCharacter").on("click", function() {
+        name.innerHTML = '<em>Loading...</em>';
+        eyecolor.innerHTML = '<em>Loading...</em>';
+        picture.innerHTML = '<em>Loading...</em>';
+        
         const randomId = Math.ceil(Math.random() * 88);
-        console.log( $(randomId) );
+        $.get(`https://akabab.github.io/starwars-api/api/id/${randomId}.json`, function(data){
+            $("div#name").text(data['name']);
+            $("div#eyecolor").text(data['eyeColor']);
+            $("#picture").attr("src", data['image']);
+        })
     })
 })
