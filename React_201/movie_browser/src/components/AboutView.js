f9cfc59c361436747914fc1bb1713646acf5745b
ZaeemT <zaeemtauqir@hotmail.com>
2022-07-29 23:38:41 +0500

    Started movie browser using react js.

---
+++ React_201/movie_browser/src/components/AboutView.js
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+
+const AboutView = () => {
+    return (
+      <>
+        <Hero />
+        <h2>About Us.</h2>
+      </>
+    )
+  }
+
+export default AboutView; 
---
+++ React_201/movie_browser/src/components/Home.js
@@ -0,0 +1,12 @@
+import Hero from "./Hero";
+
+const Home = () => {
+    return (
+        <>
+            <Hero />
+            <h1>This is home page.</h1>
+        </>
+    )        
+}
+
+export default Home;
