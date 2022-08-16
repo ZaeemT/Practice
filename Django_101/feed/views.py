dce327f0d97d64c84918ec534c9e37b36a9d7b8b
ZaeemT <zaeemtauqir@hotmail.com>
2022-08-16 19:06:26 +0500

    Post could ne uplaoded.

--- Django_101/feed/views.py
+++ Django_101/feed/views.py
@@ -1,4 +1,6 @@
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, DetailView, FormView
+
+from .forms import PostForm
 from .models import Post
 # Create your views here.
 
@@ -9,3 +11,22 @@ class HomePageView(TemplateView):
         context = super().get_context_data(**kwargs)
         context['posts'] = Post.objects.all()
         return context
+
+
+class PostDetailView(DetailView):
+    template_name = "detail.html"
+    model = Post
+
+
+class AddPostView(FormView):
+    template_name = "new_post.html"
+    form_class = PostForm
+    success_url = "/"
+
+    def form_valid(self, form):
+        #Create new post
+        new_object = Post.objects.create(
+            text = form.cleaned_data['text'],
+            image = form.cleaned_data['image']
+        )
+        return super().form_valid(form)
