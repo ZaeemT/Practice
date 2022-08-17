705b7206a6375cb9fe3bebc4264289b746657b30
ZaeemT <zaeemtauqir@hotmail.com>
2022-08-17 18:28:14 +0500

    Added bootstrap to django project.

--- Django_101/feed/views.py
+++ Django_101/feed/views.py
@@ -1,3 +1,4 @@
+from django.contrib import messages
 from django.views.generic import TemplateView, DetailView, FormView
 
 from .forms import PostForm
@@ -9,7 +10,7 @@ class HomePageView(TemplateView):
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
-        context['posts'] = Post.objects.all()
+        context['posts'] = Post.objects.all().order_by('-id')
         return context
 
 
@@ -23,10 +24,15 @@ class AddPostView(FormView):
     form_class = PostForm
     success_url = "/"
 
+    def dispatch(self, request, *args, **kwargs):
+        self.request = request
+        return super().dispatch(request, *args, **kwargs)
+
     def form_valid(self, form):
         #Create new post
         new_object = Post.objects.create(
             text = form.cleaned_data['text'],
             image = form.cleaned_data['image']
         )
+        messages.add_message(self.request, messages.SUCCESS, 'Your post was successful!')
         return super().form_valid(form)
